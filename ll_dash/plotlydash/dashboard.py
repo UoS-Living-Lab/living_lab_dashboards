4dec81a07c6453ce795af6cb76ffaa6dae062d7b
Ethan Bellmer <e.bellmer@outlook.com>
2021-11-08 10:56:24 +0000

    Add mroe testing charts

--- ll_dash/plotlydash/dashboard.py
+++ ll_dash/plotlydash/dashboard.py
@@ -11,7 +11,6 @@ from .layout import html_layout
 
 
 def init_dashboard(server):
-	"""Create a Plotly Dash dashboard."""
 	dash_app = dash.Dash(
 		server=server,
 		routes_pathname_prefix='/dashapp/',
@@ -28,8 +27,8 @@ def init_dashboard(server):
 	dash_app.index_string = html_layout
 
 
-	def update_ttn_scatter():
-		df = get_ttn_data("Temperature")
+	def ttn_scatter(sensor):
+		df = get_ttn_data(sensor)
 		X = df['received_at']
 		Y = df['sensor_value']
 
@@ -45,19 +44,19 @@ def init_dashboard(server):
 
 		return {'data': [data],'layout' : [layout]}
 
-	def update_sel_scatter():
-		df = get_sel_data("Moisture Sensor 1")
+	def sel_scatter(sensor, unit):
+		df = get_sel_data(sensor, unit)
 		X = df['lastUpdate']
 		Y = df['readingValue']
 
 		data = go.Scatter(
 				x=list(X),
 				y=list(Y),
-				name='Moisture Sensor 1',
+				name=sensor,
 				mode= 'lines+markers'
 				)
 		layout = go.Layout(
-			title = 'Moisture Sensor 1'
+			title = sensor
 			)
 
 		return {'data': [data],'layout' : [layout]}
@@ -65,39 +64,17 @@ def init_dashboard(server):
 
 	# Create Layout
 	dash_app.layout = html.Div(
-		children=[dcc.Graph(
-			id='histogram-graph',
-			figure={
-				'data': [{
-					'x': df['complaint_type'],
-					'text': df['complaint_type'],
-					'customdata': df['key'],
-					'name': '311 Calls by region.',
-					'type': 'histogram'
-				}],
-				'layout': {
-					'title': 'NYC 311 Calls category.',
-					'height': 500,
-					'padding': 150
-				}
-			}),
-			dcc.Graph(id='ttn', figure = update_ttn_scatter()),
-			dcc.Graph(id='sel', figure = update_sel_scatter()),
-			create_data_table(df)
+		children=[
+			dcc.Graph(id='ttn-temp', figure = ttn_scatter("Temperature")),
+			dcc.Graph(id='sel-cp2-ms1', figure = sel_scatter("Moisture Sensor 1", "Control Panel 2 PH1")),
+			dcc.Graph(id='sel-cp2-ms2', figure = sel_scatter("Moisture Sensor 2", "Control Panel 2 PH1")),
+			dcc.Graph(id='sel-cp2-l1', figure = sel_scatter("Level 1", "Control Panel 2 PH1")),
+			dcc.Graph(id='sel-lw-graw', figure = sel_scatter("Grey Wall", "Live Wall PH2")),
+			dcc.Graph(id='sel-lw-grew', figure = sel_scatter("Green Wall", "Live Wall PH2")),
+			dcc.Graph(id='sel-lw-redw', figure = sel_scatter("Red Wall", "Live Wall PH2")),
+			dcc.Graph(id='sel-lw-bluw', figure = sel_scatter("Blue Wall", "Live Wall PH2")),
 		],
 		id='dash-container'
 	)
 	return dash_app.server
 
-
-def create_data_table(df):
-	"""Create Dash datatable from Pandas DataFrame."""
-	table = dash_table.DataTable(
-		id='database-table',
-		columns=[{"name": i, "id": i} for i in df.columns],
-		data=df.to_dict('records'),
-		sort_action="native",
-		sort_mode='native',
-		page_size=300
-	)
-	return table
