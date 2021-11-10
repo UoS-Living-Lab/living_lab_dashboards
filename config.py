971060cebfc3d9b209120ec52369eec2a6b6bb1f
Ethan Bellmer <e.bellmer@outlook.com>
2021-11-10 14:18:43 +0000

    Comment out optionals for testing

--- config.py
+++ config.py
@@ -7,19 +7,19 @@ BASE_DIR = path.abspath(path.dirname(__file__))
 
 
 class Config:
-    """Flask configuration variables."""
+	"""Flask configuration variables."""
 
-    # General Config
-    FLASK_APP = config("FLASK_APP")
-    FLASK_ENV = config("FLASK_ENV")
-    SECRET_KEY = config("SECRET_KEY")
+	# General Config
+	FLASK_APP = config("FLASK_APP")
+	FLASK_ENV = config("FLASK_ENV")
+	SECRET_KEY = config("SECRET_KEY")
 
-    # Assets
-    LESS_BIN = config("LESS_BIN")
-    ASSETS_DEBUG = config("ASSETS_DEBUG")
-    LESS_RUN_IN_DEBUG = config("LESS_RUN_IN_DEBUG")
+	# Assets
+	#LESS_BIN = config("LESS_BIN")
+	#ASSETS_DEBUG = config("ASSETS_DEBUG")
+	#LESS_RUN_IN_DEBUG = config("LESS_RUN_IN_DEBUG")
 
-    # Static Assets
-    STATIC_FOLDER = "static"
-    TEMPLATES_FOLDER = "templates"
-    COMPRESSOR_DEBUG = config("COMPRESSOR_DEBUG")
+	# Static Assets
+	STATIC_FOLDER = "static"
+	TEMPLATES_FOLDER = "templates"
+	COMPRESSOR_DEBUG = config("COMPRESSOR_DEBUG")
